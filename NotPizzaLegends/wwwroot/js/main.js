ceb909945ecdcfa6146ec98c71bad047d1249565
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-02-27 12:18:18 -0500

    use js interop for drawing on canvas

---
+++ NotPizzaLegends/wwwroot/js/main.js
@@ -0,0 +1,11 @@
+﻿function drawImage(canvas, src, x, y) {
+
+    ctx = canvas.getContext("2d");
+
+    const image = new Image();
+    image.onload = () => {
+        ctx.drawImage(image, x, y)
+    };
+
+    image.src = src;
+}
