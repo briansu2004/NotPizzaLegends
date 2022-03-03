9754164401d64b56a2c94f4ebbfd8d8130744d57
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-03-03 09:04:09 -0500

    add movement but badly

--- NotPizzaLegends/Scenes/Scene.cs
+++ NotPizzaLegends/Scenes/Scene.cs
@@ -9,6 +9,11 @@ public interface IScene
     IMap? Map { get; }
     List<ISprite?> Sprites { get; }
     bool IsRunning { get; }
+
+    void MoveDown();
+    void MoveLeft();
+    void MoveRight();
+    void MoveUp();
 }
 
 public abstract class Scene : ComponentBase, IScene
@@ -17,4 +22,9 @@ public abstract class Scene : ComponentBase, IScene
     public List<ISprite?> Sprites { get; } = new();
 
     public bool IsRunning { get; protected set; }
+
+    public abstract void MoveUp();
+    public abstract void MoveDown();
+    public abstract void MoveLeft();
+    public abstract void MoveRight();
 }
